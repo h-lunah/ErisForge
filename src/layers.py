4b5a5fac9a5cf99cf0ad3c0f0b831c3117bc2112
tsadoq <github.v5331@passmail.net>
2024-11-09 16:13:30 +0100

    docstrings, isorting, ruff and co

--- src/layers.py
+++ src/layers.py
@@ -6,12 +6,24 @@ import torch
 
 
 class AblationDecoderLayer(torch.nn.Module):
+
     def __init__(self, original_layer, direction):
+        """
+        Initialize the AblationDecoderLayer.
+        :param original_layer: the original layer to be ablated
+        :param direction: the direction of the ablation.
+        """
         super(AblationDecoderLayer, self).__init__()
         self.original_layer = original_layer
         self.objective_behaviour_dr = direction
 
     def forward(self, *args, **kwargs):
+        """
+        Forward method of the AblationDecoderLayer.
+        :param args: the arguments to be passed to the original layer
+        :param kwargs: the keyword arguments to be passed to the original layer
+        :return: the output of the original layer after ablation.
+        """
         hidden_states = args[0]
         ablated = self._direction_ablation_hook(
             activation=hidden_states,
@@ -25,17 +37,34 @@ class AblationDecoderLayer(torch.nn.Module):
             activation: jaxtyping.Float[torch.Tensor, "... d_act"],
             direction: jaxtyping.Float[torch.Tensor, "d_act"]
     ) -> torch.Tensor:
+        """
+        Ablation hook for the AblationDecoderLayer.
+        :param activation: the activation to be ablated
+        :param direction: the direction of the ablation
+        :return: the ablated activation
+        """
         proj = einops.einsum(activation, direction.view(-1, 1), '... d_act, d_act single -> ... single') * direction
         return activation - proj
 
 
 class AdditionDecoderLayer(torch.nn.Module):
     def __init__(self, original_layer, direction):
+        """
+        Initialize the AdditionDecoderLayer.
+        :param original_layer: the original layer to be added to
+        :param direction: the direction of the addition.
+        """
         super(AdditionDecoderLayer, self).__init__()
         self.original_layer = original_layer
         self.objective_behaviour_dir = direction
 
     def forward(self, *args, **kwargs):
+        """
+        Forward method of the AdditionDecoderLayer.
+        :param args: the arguments to be passed to the original layer
+        :param kwargs: the keyword arguments to be passed to the original layer
+        :return: the output of the original layer after addition.
+        """
         hidden_states = args[0]
         added = self._direction_addition_hook(
             activation=hidden_states,
@@ -49,5 +78,11 @@ class AdditionDecoderLayer(torch.nn.Module):
             activation: jaxtyping.Float[torch.Tensor, "... d_act"],
             direction: jaxtyping.Float[torch.Tensor, "d_act"]
     ) -> torch.Tensor:
+        """
+        Addition hook for the AdditionDecoderLayer.
+        :param activation: the activation to be added to
+        :param direction: the direction of the addition
+        :return: the added activation
+        """
         return activation + direction
 
